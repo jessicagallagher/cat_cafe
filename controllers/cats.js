af1e18704fcc278def590fc2432b72bbffe292cb
jessicagallagher <jessicalyngallagher@me.com>
2021-02-28 20:15:42 -0500

    added some cats to db, rendering correctly and styled on index.ejs, created show page which is rendering and connected

--- controllers/cats.js
+++ controllers/cats.js
@@ -9,8 +9,11 @@ const Cat = require('../models/catschema.js');
 
 // index
 cats.get('/', (req, res) => {
-	res.render('index.ejs', {
-		tabTitle: 'Home'
+	Cat.find({}, (error, allCats) => {
+		res.render('index.ejs', {
+			tabTitle: 'Home',
+			cats: allCats
+		});
 	});
 });
 
@@ -25,6 +28,16 @@ cats.get('/new', (req, res) => {
 cats.post('/', (req, res) => {
 	Cat.create(req.body, (err, createdCat) => {
 		res.redirect('/cats');
+	});
+});
+
+// show
+cats.get('/:id', (req, res) => {
+	Cat.findById(req.params.id, (err, foundCat) => {
+		res.render('show.ejs', {
+			tabTitle: foundCat.name,
+			cats: foundCat
+		});
 	});
 });
 
