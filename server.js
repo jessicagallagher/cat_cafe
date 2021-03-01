e0ff35143c8c3e13c8ed4b8108edd5e4749519bd
jessicagallagher <jessicalyngallagher@me.com>
2021-03-01 11:00:02 -0500

    heroku stuff

--- server.js
+++ server.js
@@ -2,20 +2,38 @@
 const express = require('express');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
+const db = mongoose.connection;
 
 // app config
 const app = express();
-const port = 3000;
-const databaseName = 'cat_cafe';
+const port = process.env.port || 3000;
+// const databaseName = 'cat_cafe';
 
 // controller
 const catController = require('./controllers/cats.js');
 
+//___________________
+//Database
+//___________________
+// How to connect to the database either via heroku or locally
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/' + `cat_cafe`;
+
+// Connect to Mongo
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+// open the connection to mongo
+db.on('open', () => {});
+
 // database config
-mongoose.connect(`mongodb://localhost:27017/${databaseName}`, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
-mongoose.connection.once('open', () => {
-	console.log('connected to mongo');
-});
+// mongoose.connect(`mongodb://localhost:27017/${databaseName}`, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+// mongoose.connection.once('open', () => {
+// 	console.log('connected to mongo');
+// });
 
 // middleware
 app.use(express.urlencoded({ extended: false }));
